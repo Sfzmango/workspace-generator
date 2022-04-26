b1d5efca873e99ea95fce3cafb3019b3b78a414e
Maung Htike <maunghtike1999@gmail.com>
2022-04-25 19:26:57 -0700

    add placeholder employee class

---
+++ classes/employee.js
@@ -0,0 +1,33 @@
+class Employee {
+    // Just like constructor functions, classes can accept arguments
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    role = "Employee";
+
+    getName() {
+        console.log(this.name);
+    }
+
+    getId() {
+        console.log(this.id);
+    }
+
+    getEmail() {
+        console.log(this.email);
+    }
+
+    getRole() {
+        console.log(this.role);
+    }
+}
+
+const joey = new Employee("Joey", 12345, "joey@gmail.com");
+
+joey.getName();
+joey.getId();
+joey.getEmail();
+joey.getRole();
