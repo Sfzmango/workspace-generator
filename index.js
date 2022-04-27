cf71db5d7721e35e2fc4e7dfb08e25ea88544033
Maung Htike <maunghtike1999@gmail.com>
2022-04-27 11:15:27 -0700

    add functionality to inquirer

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const rolesArr = ["Engineer", "Intern", "Done"];
 const engineerArr = [];
 const internArr = [];
 
+const util = require('util')
+
 // loops if the user is not done inputting people to the workspace
-async function askRole() {
+async function askRole(manager) {
+
     inquirer
         .prompt([
             {
@@ -51,9 +54,10 @@ async function askRole() {
                         var engineerInfo = response;
                         engineerArr.push(new Engineer(`${engineerInfo.name}`, `${engineerInfo.id}`, `${engineerInfo.email}`, `${engineerInfo.github}`));
                         console.log(engineerArr);
-                        askRole();
+                        askRole(manager);
                     })
             }
+
             else if (ans.selectedRole === "Intern") {
                 inquirer
                     .prompt([
@@ -82,46 +86,64 @@ async function askRole() {
                         var internInfo = response;
                         internArr.push(new Intern(`${internInfo.name}`, `${internInfo.id}`, `${internInfo.email}`, `${internInfo.school}`));
                         console.log(internArr);
-                        askRole();
+                        askRole(manager);
                     })
+            }
+
+            if (ans.selectedRole === "Done") {
+                console.dir({
+                    'The following users have been created': { engineers: engineerArr, interns: JSON.stringify(internArr), manager }
+                })
+                console.log(util.inspect({ 'The following users have been created': { engineers: engineerArr, interns: JSON.stringify(internArr), manager } }, true, 10))
+                return { engineerArr, internArr, manager }
             }
         })
 }
+async function start() {
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is the manager's name?",
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is the manager's email?",
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "What is the manager's ID?",
+            },
+            {
+                name: "officeNumber",
+                type: "input",
+                message: "What is the manager's office Number?",
+            },
+            {
+                name: "selectedRole",
+                type: "list",
+                message: "Confirm that this is the manager?",
+                choices: ["Manager"]
+            }
+        ])
+        .then((response) => {
+            var managerInfo = response;
+            const teamManager = new Manager(`${managerInfo.name}`, `${managerInfo.id}`, `${managerInfo.email}`, `${managerInfo.officeNumber}`);
+            console.log({ manager: teamManager });
 
-inquirer
-    .prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "What is the manager's name?",
-        },
-        {
-            name: "email",
-            type: "input",
-            message: "What is the manager's email?",
-        },
-        {
-            name: "id",
-            type: "input",
-            message: "What is the manager's ID?",
-        },
-        {
-            name: "officeNumber",
-            type: "input",
-            message: "What is the manager's office Number?",
-        },
-        {
-            name: "selectedRole",
-            type: "list",
-            message: "Confirm that this is the manager?",
-            choices: ["Manager"]
-        }
-    ])
-    .then((response) => {
-        var managerInfo = response;
-        const teamManager = new Manager(`${managerInfo.name}`, `${managerInfo.id}`, `${managerInfo.email}`, `${managerInfo.officeNumber}`);
-        console.log(teamManager);
-        askRole()
-    })
+            return teamManager;
+        }).then((manager) => {
+            console.log(manager);
+            askRole(manager)
+        })
+    // .then((response) => {
+    //     console.log({ response: response })
+    // })
 
+}
+
+start();
 
